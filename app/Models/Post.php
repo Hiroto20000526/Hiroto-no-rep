7a94ba2619cca4b87886e022e9d7b0a868f5331d
Hiroto20000526 <hirofireo@gmail.com>
2023-10-05 16:24:30 +0900

    カリキュラム8-4

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,8 +9,15 @@ class Post extends Model
 {
     use HasFactory;
     
+    protected $fillable = [
+        'title',
+        'body'
+        ];
+    
     public function getPaginateByLimit(int $limit_count = 5)
     {
-        return $this->orderBy('update_at', 'DESC')->paginate($limit_count);
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
+    
+    
 }
